f0ad47d2de93bd9ed9f8fb0b0e2d18800c3f172c
lexusjhony <107890683+lexusjhony@users.noreply.github.com>
2022-11-03 15:40:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Learning English</title>
+
+	<!--CSS-->
+	<link href="style/style.css" rel="stylesheet" type="text/css">
+	</link>
+
+</head>
+
+<body>
+
+	<div>
+		<div class="juego1">
+			<h1 align="center">
+				Number
+			</h1>
+		</div>
+		<div class="juego2">
+			<h1 align="center">
+				Letters
+			</h1>
+		</div>
+	</div>
+
+
+	<div>
+		<div class="juego3">
+			<h1 align="center">
+				Colors
+			</h1>
+		</div>
+		<div class="juego4">
+			<h1 align="center">
+				Animals
+			</h1>
+		</div>
+	</div>
+
+
+</body>
+
+</html>
